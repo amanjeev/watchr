6cb1f9f3afe34873550af41b1fd57ff4ac5bd8e0
Amanjeev Sethi <aj@amanjeev.com>
2021-12-19 16:43:48 -0500

    clap arg added for the command, reads cli arg

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+use clap::{App, Arg};
+
 fn main() {
-    println!("Hello, world!");
+    // TODO: add an option to not preserve history (by default we preserve history in this tool)
+    let matches = App::new("watchr")
+        .setting(clap::AppSettings::TrailingVarArg)
+        .arg(
+            Arg::new("command")
+                .multiple_occurrences(true)
+                .required(true)
+                .takes_value(true)
+                .help("Command to watch over"),
+        )
+        .get_matches();
+
+    let command: Vec<&str> = matches.values_of("command").unwrap().collect();
+
+    // TODO: watch over the command saved in `command` above
+
+    println!("{:?}", command);
 }
